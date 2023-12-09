b766a1be16815c680f250868409b105e76c41814
satake <satake01_21@yahoo.co.jp>
2023-12-10 07:22:29 +0900

    進捗管理バーの実装

--- 開発用/src/assets/js/script.js
+++ 開発用/src/assets/js/script.js
@@ -116,11 +116,11 @@ if (spendWrapper && window.innerWidth > 768) {
 function ScrollTimelineAnime(){
   $('.p-spend-slide__block').each(function(){// それぞれのli要素の
     var elemPos = $(this).offset().left;// 上からの高さ取得
-    var scroll = $(window).scrollTop();// スクロール値取得
+    var scroll = $(window).scrollLeft();// スクロール値取得
     var windowWidth = $(window).width();// windowの高さ取得
     var startPoint = 100; //線をスタートさせる位置を指定※レイアウトによって調整してください
     if (scroll >= elemPos - windowWidth-startPoint){       
-      var H = $(this).outerHeight(true)//liの余白と高さを含めた数値を取得
+      var H = $(this).outerWidth(true)//liの余白と高さを含めた数値を取得
       //スクロール値から要素までの高さを引いた値を、liの高さの半分のパーセントで出す
       var percent = (scroll+startPoint - elemPos) / (H/2) *100;//liの余白と高さの半分で線を100％に伸ばす
 
